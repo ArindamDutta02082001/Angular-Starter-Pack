37d007b6d75878ecba19ba403633fc759bfd81dc
ArindamDutta02082001 <83761396+ArindamDutta02082001@users.noreply.github.com>
2024-03-24 10:39:22 +0530

    angular till lifecycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'project';
+
+  // @Input decorators parameters
+  compData: string = 'Hi from parent component data';
+  person: Object[] = [
+    {
+      name: 'Katsuko Saruhashi',
+      imageId: 'YfeOqp2',
+    },
+  ];
+
+  // @Output decorator function
+  childData : string = "";
+  addItem(data : string)
+  {
+    this.childData = data;
+  }
+
+  items = [
+    { name: 'Item 1', isValid: true },
+    { name: 'Item 2', isValid: false },
+    { name: 'Item 3', isValid: true },
+    { name: 'Item 4', isValid: false },
+    { name: 'Item 5', isValid: true },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Header_Bindings/header.component';
+import { DirectivesComponent } from './directives/directives.component';
+import { NgContentComponent } from './ng-content/ng-content.component';
+import { LifecycleHooksComponent } from './lifecycle-hooks/lifecycle-hooks.component';
+import { DataPassingComponent } from './data-passing/data-passing.component';
+import { ConstructorComponent } from './lifecycle-hooks/constructor/constructor.component';
+import { OnChangeComponent } from './lifecycle-hooks/on-change/on-change.component';
+import { OnInitComponent } from './lifecycle-hooks/on-init/on-init.component';
+import { NgDoCheckComponent } from './lifecycle-hooks/ng-do-check/ng-do-check.component';
+import { NgAftercontentinitComponent } from './lifecycle-hooks/ng-aftercontentinit/ng-aftercontentinit.component';
+import { BabyComponentComponent } from './lifecycle-hooks/ng-aftercontentinit/baby-component/baby-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DirectivesComponent,
+    NgContentComponent,
+    LifecycleHooksComponent,
+    DataPassingComponent,
+    ConstructorComponent,
+    OnChangeComponent,
+    OnInitComponent,
+    NgDoCheckComponent,
+    NgAftercontentinitComponent,
+    BabyComponentComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
