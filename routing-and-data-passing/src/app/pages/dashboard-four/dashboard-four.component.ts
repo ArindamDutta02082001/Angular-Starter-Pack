c4b9e34a996e318fac52b88a9cffb26ade5c2fbd
ArindamDutta02082001 <83761396+ArindamDutta02082001@users.noreply.github.com>
2024-07-18 22:15:44 +0530

    commit

---
+++ routing-and-data-passing/src/app/pages/dashboard-four/dashboard-four.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-four',
+  templateUrl: './dashboard-four.component.html',
+  styleUrl: './dashboard-four.component.css'
+})
+export class DashboardFourComponent {
+
+}
